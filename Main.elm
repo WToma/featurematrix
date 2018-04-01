eb06f815dfbe367a38e19647e74cf69c9e1e9f98
Tamas Weisz <weisztoma@gmail.com>
2018-04-01 16:27:08 -0700

    Able to edit intersections, and saved into model (not shown yet)

--- Main.elm
+++ Main.elm
@@ -1,18 +1,21 @@
 module Main exposing (..)
 
 import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
-import Html.Attributes exposing (class)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (class, type_, id)
+import Dict exposing (Dict)
 
 
-main : Program Never number Msg
+main : Program Never Model Msg
 main =
-    Html.beginnerProgram { model = 0, view = view, update = update }
+    Html.beginnerProgram { model = { intersections = Dict.empty }, view = view, update = update }
 
+type alias Model = {
+    intersections: Dict (String, String) String
+}
 
 type Msg
-    = Increment
-    | Decrement
+    = IntersectionUpdated String String String
 
 
 type alias Feature =
@@ -25,38 +28,46 @@ type alias Feature =
 dummyFeatures : List Feature
 dummyFeatures =
     [ { featureId = "showFeatures"
-      , displayName = "Shows Features as a Table"
+      , displayName = "Shows Features"
       , description = "Shows features as a Table"
       }
-    , { featureId = "focus"
-      , displayName = "Focus on Selected Features"
-      , description = "Select which features to display"
+    , { featureId = "edit"
+      , displayName = "Edit Intersection"
+      , description = "Type Features into the Cells of the Table"
       }
     ]
 
 
 renderFeatureTable : List Feature -> Html msg
-renderFeatureTable features =
+renderFeatureTable = renderFeatureTableGeneric (\_ -> Html.text "")
+
+renderFeatureTableGeneric : ((Feature, Feature) -> Html msg) -> List Feature -> Html msg
+renderFeatureTableGeneric intersectionRenderer features =
     Html.table [ class "featureTable" ]
         (-- header
          [ Html.tr [] ([ Html.th [ class "featureTable" ] [] ] ++ List.map (\f -> Html.th [ class "featureTable" ] [ Html.text f.displayName ]) features) ]
             ++ -- rows
                List.map
                 (\f ->
-                    Html.tr [] ([ Html.th [ class "featureTable" ] [ Html.text f.displayName ] ] ++ List.map (\f2 -> Html.td [ class "featureTable" ] []) features)
+                    Html.tr [] ([ Html.th [ class "featureTable" ] [ Html.text f.displayName ] ] ++ List.map (\f2 -> Html.td [ class "featureTable" ] [intersectionRenderer (f, f2)]) features)
                 )
                 features
         )
 
+renderIntersectionEditBox: (Feature, Feature) -> Html Msg
+renderIntersectionEditBox (f1, f2) =
+    let
+        smallerId = if f1.featureId < f2.featureId then f1.featureId else f2.featureId
+        largerId = if f1.featureId < f2.featureId then f2.featureId else f1.featureId
+        inputId = smallerId ++ "_vs_" ++ largerId
+    in
+        Html.input [type_ "text", id inputId, onInput (IntersectionUpdated smallerId largerId)] []
 
-update : Msg -> number -> number
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            model + 1
-
-        Decrement ->
-            model - 1
+        IntersectionUpdated smallerId largerId newValue ->
+            { intersections = Dict.insert (smallerId, largerId) newValue model.intersections }
 
 
 view : a -> Html Msg
@@ -66,4 +77,5 @@ view model =
         -- , div [] [ text (toString model) ]
         -- , button [ onClick Increment ] [ text "+" ]
         -- ]
-        [ renderFeatureTable dummyFeatures ]
+        -- [ renderFeatureTable dummyFeatures ]
+        [ renderFeatureTableGeneric renderIntersectionEditBox dummyFeatures]
