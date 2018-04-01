a1598ad6f78ef7451f07ffb97c7425922f907e2c
Tamas Weisz <weisztoma@gmail.com>
2018-04-01 15:29:43 -0700

    Moving away from elm-reactor and using an external stylesheet for styling the table

--- Main.elm
+++ Main.elm
@@ -4,6 +4,7 @@ import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (class)
 
+
 main : Program Never number Msg
 main =
     Html.beginnerProgram { model = 0, view = view, update = update }
@@ -20,32 +21,33 @@ type alias Feature =
     , description : String
     }
 
+
 dummyFeatures : List Feature
-dummyFeatures = [
-    {
-        featureId = "showFeatures",
-        displayName = "Shows Features as a Table",
-        description = "Shows features as a Table"
-    },
-    {
-        featureId = "focus",
-        displayName = "Focus on Selected Features",
-        description=  "Select which features to display"
-    }
- ]
+dummyFeatures =
+    [ { featureId = "showFeatures"
+      , displayName = "Shows Features as a Table"
+      , description = "Shows features as a Table"
+      }
+    , { featureId = "focus"
+      , displayName = "Focus on Selected Features"
+      , description = "Select which features to display"
+      }
+    ]
+
 
 renderFeatureTable : List Feature -> Html msg
 renderFeatureTable features =
     Html.table [ class "featureTable" ]
         (-- header
-         [ Html.tr [] ([ Html.th [] [] ] ++ List.map (\f -> Html.th [] [ Html.text f.displayName ]) features) ]
+         [ Html.tr [] ([ Html.th [ class "featureTable" ] [] ] ++ List.map (\f -> Html.th [ class "featureTable" ] [ Html.text f.displayName ]) features) ]
             ++ -- rows
                List.map
                 (\f ->
-                    Html.tr [] ([ Html.th [] [ Html.text f.displayName ] ] ++ List.map (\f2 -> Html.td [] []) features)
+                    Html.tr [] ([ Html.th [ class "featureTable" ] [ Html.text f.displayName ] ] ++ List.map (\f2 -> Html.td [ class "featureTable" ] []) features)
                 )
                 features
         )
+
 
 update : Msg -> number -> number
 update msg model =
@@ -56,6 +58,7 @@ update msg model =
         Decrement ->
             model - 1
 
+
 view : a -> Html Msg
 view model =
     div []
@@ -63,4 +66,4 @@ view model =
         -- , div [] [ text (toString model) ]
         -- , button [ onClick Increment ] [ text "+" ]
         -- ]
-        [renderFeatureTable dummyFeatures]
+        [ renderFeatureTable dummyFeatures ]
