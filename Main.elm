501272adc5e19d955707f763f66cd8e58c179c20
Tamas Weisz <weisztoma@gmail.com>
2018-04-07 16:11:20 -0700

    Formatting Removing empty keys on update

--- Main.elm
+++ Main.elm
@@ -10,9 +10,11 @@ main : Program Never Model Msg
 main =
     Html.beginnerProgram { model = { intersections = Dict.empty }, view = view, update = update }
 
-type alias Model = {
-    intersections: Dict (String, String) String
-}
+
+type alias Model =
+    { intersections : Dict ( String, String ) String
+    }
+
 
 type Msg
     = IntersectionUpdated String String String
@@ -39,9 +41,11 @@ dummyFeatures =
 
 
 renderFeatureTable : List Feature -> Html msg
-renderFeatureTable = renderFeatureTableGeneric (\_ -> Html.text "")
+renderFeatureTable =
+    renderFeatureTableGeneric (\_ -> Html.text "")
 
-renderFeatureTableGeneric : ((Feature, Feature) -> Html msg) -> List Feature -> Html msg
+
+renderFeatureTableGeneric : (( Feature, Feature ) -> Html msg) -> List Feature -> Html msg
 renderFeatureTableGeneric intersectionRenderer features =
     Html.table [ class "featureTable" ]
         (-- header
@@ -49,30 +53,49 @@ renderFeatureTableGeneric intersectionRenderer features =
             ++ -- rows
                List.map
                 (\f ->
-                    Html.tr [] ([ Html.th [ class "featureTable" ] [ Html.text f.displayName ] ] ++ List.map (\f2 -> Html.td [ class "featureTable" ] [intersectionRenderer (f, f2)]) features)
+                    Html.tr [] ([ Html.th [ class "featureTable" ] [ Html.text f.displayName ] ] ++ List.map (\f2 -> Html.td [ class "featureTable" ] [ intersectionRenderer ( f, f2 ) ]) features)
                 )
                 features
         )
 
-renderIntersectionEditBox: Dict (String, String) String -> (Feature, Feature) -> Html Msg
-renderIntersectionEditBox intersectionValues (f1, f2) =
+
+renderIntersectionEditBox : Dict ( String, String ) String -> ( Feature, Feature ) -> Html Msg
+renderIntersectionEditBox intersectionValues ( f1, f2 ) =
     let
-        smallerId = if f1.featureId < f2.featureId then f1.featureId else f2.featureId
-        largerId = if f1.featureId < f2.featureId then f2.featureId else f1.featureId
-        inputId = smallerId ++ "_vs_" ++ largerId
-        intersectionValue = Maybe.withDefault "" (Dict.get (smallerId, largerId) intersectionValues)
+        smallerId =
+            if f1.featureId < f2.featureId then
+                f1.featureId
+            else
+                f2.featureId
+
+        largerId =
+            if f1.featureId < f2.featureId then
+                f2.featureId
+            else
+                f1.featureId
+
+        inputId =
+            smallerId ++ "_vs_" ++ largerId
+
+        intersectionValue =
+            Maybe.withDefault "" (Dict.get ( smallerId, largerId ) intersectionValues)
     in
-        Html.input [type_ "text", id inputId, value intersectionValue, onInput (IntersectionUpdated smallerId largerId)] []
+        Html.input [ type_ "text", id inputId, value intersectionValue, onInput (IntersectionUpdated smallerId largerId) ] []
+
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         IntersectionUpdated smallerId largerId newValue ->
-            { intersections = Dict.insert (smallerId, largerId) newValue model.intersections }
+            if newValue /= "" then
+                { intersections = Dict.insert ( smallerId, largerId ) newValue model.intersections }
+            else
+                { intersections = Dict.remove ( smallerId, largerId ) model.intersections }
 
 
 view : Model -> Html Msg
 view model =
-    div [class "featureTableContainer"]
+    div [ class "featureTableContainer" ]
         [ renderFeatureTableGeneric (renderIntersectionEditBox model.intersections) dummyFeatures
-        , text (toString model.intersections)]
+        , text (toString model.intersections)
+        ]
