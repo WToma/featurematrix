ba0731290904ea0746d41a70a69cf661165fae89
Tamas Weisz <weisztoma@gmail.com>
2018-04-07 17:02:16 -0700

    Button to show the JSON model

--- Main.elm
+++ Main.elm
@@ -1,24 +1,27 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, text, textarea)
 import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (class, type_, id, value)
+import Html.Attributes exposing (class, type_, id, value, readonly)
 import Dict exposing (Dict)
 import Json.Encode as JE
 
 
 main : Program Never Model Msg
 main =
-    Html.beginnerProgram { model = { intersections = Dict.empty }, view = view, update = update }
+    Html.beginnerProgram { model = { intersections = Dict.empty, showSerialized = False }, view = view, update = update }
 
 
 type alias Model =
     { intersections : Dict ( String, String ) String
+    , showSerialized : Bool
     }
 
 
 type Msg
     = IntersectionUpdated String String String
+    | ShowModel
+    | HideModel
 
 
 type alias Feature =
@@ -37,6 +40,14 @@ dummyFeatures =
     , { featureId = "edit"
       , displayName = "Edit Intersection"
       , description = "Type Features into the Cells of the Table"
+      }
+    , { featureId = "export"
+      , displayName = "Export"
+      , description = "Export the Current Model into JSON"
+      }
+    , { featureId = "import"
+      , displayName = "Import"
+      , description = "Import the Model from JSON"
       }
     ]
 
@@ -89,16 +100,47 @@ update msg model =
     case msg of
         IntersectionUpdated smallerId largerId newValue ->
             if newValue /= "" then
-                { intersections = Dict.insert ( smallerId, largerId ) newValue model.intersections }
+                { model | intersections = Dict.insert ( smallerId, largerId ) newValue model.intersections }
             else
-                { intersections = Dict.remove ( smallerId, largerId ) model.intersections }
+                { model | intersections = Dict.remove ( smallerId, largerId ) model.intersections }
+
+        HideModel ->
+            { model | showSerialized = False }
+
+        ShowModel ->
+            { model | showSerialized = True }
 
 
 view : Model -> Html Msg
 view model =
     div [ class "featureTableContainer" ]
         [ renderFeatureTableGeneric (renderIntersectionEditBox model.intersections) dummyFeatures
-        , text (encodeModel model.intersections)
+        , renderModelInputOutput model
+        ]
+
+
+renderModelInputOutput : Model -> Html Msg
+renderModelInputOutput model =
+    div []
+        [ if model.showSerialized then
+            textarea [ value (encodeModel model.intersections), readonly True ] []
+          else
+            text ""
+        , button
+            [ onClick
+                (if model.showSerialized then
+                    HideModel
+                 else
+                    ShowModel
+                )
+            ]
+            [ text
+                (if model.showSerialized then
+                    "Hide"
+                 else
+                    "Show"
+                )
+            ]
         ]
 
 
