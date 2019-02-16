7e783382ff66ae3ab1566350e6069fabdb152729
Tamas Weisz <weisztoma@gmail.com>
2019-02-16 14:57:10 -0800

    Making focus mode look nicer

--- src/FocusMode.elm
+++ src/FocusMode.elm
@@ -1,7 +1,7 @@
 module FocusMode exposing (Model, init, view)
 
 import PersistentModel exposing (Feature, PersistentModel)
-import Html exposing (Html, div, text, h2)
+import Html exposing (Html, div, text, table, h2, tr, th, td)
 import Html.Attributes exposing (class)
 import Helpers exposing (ensureSingleton)
 
@@ -35,13 +35,33 @@ init persistentModel focusedFeatureId =
 view : Model -> Html msg
 view model =
     div [ class "focusModeWrapper" ]
-        [ div [ class "focusedFeature" ] [ h2 [] [ text model.focusedFeature.displayName ] ]
-        , div [ class "crossFeature" ] [ h2 [] [ text model.crossFeature.displayName ] ]
-        , div [ class "intersection" ] [ text (displayedIntersection model) ]
+        [ table [ class "focusTable" ]
+            [ tr []
+                [ th [ class "focusTable" ] [] -- empty
+                , th [ class "focusTable" ]
+                    [ -- focused feature
+                      div [ class "focusedFeature" ] [ h2 [] [ text model.focusedFeature.displayName ] ]
+                    ]
+                ]
+            , tr []
+                [ th [ class "focusTable" ]
+                    [ -- cross feature
+                      div [ class "crossFeature" ] [ h2 [] [ text model.crossFeature.displayName ] ]
+                    ]
+                , td [ class "focusTable" ]
+                    [ -- intersection
+                      div [ class "intersection" ] [ text (displayedIntersection model) ]
+                    ]
+                ]
+            ]
         ]
 
 
 
+-- [ div [ class "focusedFeature" ] [ h2 [] [ text model.focusedFeature.displayName ] ]
+-- , div [ class "crossFeature" ] [ h2 [] [ text model.crossFeature.displayName ] ]
+-- , div [ class "intersection" ] [ text (displayedIntersection model) ]
+-- ]
 -- unexported functions
 
 
